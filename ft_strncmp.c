5f17fd7a8bd6ee1975bce7991fab282c5d31a496
Wenyan Lin <116374812+wennyanlin@users.noreply.github.com>
2023-09-16 21:20:14 +0200

    simplified

---
+++ ft_strncmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wlin <wlin@student.42barcelona.>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 20:02:02 by wlin              #+#    #+#             */
+/*   Updated: 2023/09/16 21:19:30 by wlin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
+/*
+int	main()
+{
+	char *s1 = "Hello";
+	char *s2 = "Hola";
+	size_t n = 3;
+	printf("%i", ft_strncmp(s1, s2, n));
+	return (0);
+}*/
