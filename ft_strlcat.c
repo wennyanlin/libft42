c6c51472d627dd7dd64ebc042fd87b7c292ab612
Wenyan Lin <116374812+wennyanlin@users.noreply.github.com>
2023-09-16 18:52:46 +0200

    tested ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,44 +6,55 @@
 /*   By: wlin <wlin@student.42barcelona.>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 13:51:27 by wlin              #+#    #+#             */
-/*   Updated: 2023/09/14 13:51:30 by wlin             ###   ########.fr       */
+/*   Updated: 2023/09/16 18:29:56 by wlin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "libft.h"
+#include <string.h>
+
+size_t	ft_strlen(const char *p)
+{
+	size_t	i;
+
+	i = 0;
+	while (p[i])
+	{
+		i++;
+	}
+	return (i);
+}
 
 size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
-	size_t	destlen;
+	size_t	c;
 
-	i = 0;
-	j = 0;
-	while (dest[i])
-		i++;
-	destlen = i;
-	if (destlen > d)
-	while (src[j] && dstsize != 0 && j < (dstsize - j -  1))
+	c = 0;
+	i = ft_strlen(dest);
+	j = ft_strlen(src);
+	if (dstsize < i)
+		return (j + dstsize);
+	if (i < dstsize - 1 && dstsize > 0)
 	{
-		dest[i] = src[j];
-		j++;
-		i++;
+		while (src[c] && i + c < dstsize - 1)
+		{
+			dest[i + c] = src[c];
+			c++;
+		}
+		dest[i + c] = '\0';
 	}
-	dest[i] = '\0';
-	while (src[j])
-		j++;
-	return (destlen + j);
+	return (j + i);
 }
-
+/*
 int	main()
 {
-	size_t dstsize = 10;
+	size_t dstsize = 8;
 	char *src = "789";
-	char dest[20] = "122242424242342";//
+	char dest[20] = "12345678";
 	printf("%zu\n", ft_strlcat(dest, src, dstsize));
-	//printf("%zu", strlcat(dest, src, dstsize));
+	printf("%zu\n", strlcat(dest, src, dstsize));
 	printf("%s\n", dest);
 	return (0);
-}
+}*/
