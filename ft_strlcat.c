2695491b14de360a9ade5a5dd3a45cba5e5169cf
Wenyan Lin <116374812+wennyanlin@users.noreply.github.com>
2023-10-01 21:11:23 +0200

    added explanation

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,48 +6,44 @@
 /*   By: wlin <wlin@student.42barcelona.>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 13:51:27 by wlin              #+#    #+#             */
-/*   Updated: 2023/09/16 18:29:56 by wlin             ###   ########.fr       */
+/*   Updated: 2023/10/01 21:09:07 by wlin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
-
-size_t	ft_strlen(const char *p)
-{
-	size_t	i;
-
-	i = 0;
-	while (p[i])
-	{
-		i++;
-	}
-	return (i);
-}
 
 size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 {
-	size_t	i;
-	size_t	j;
+	size_t	dest_len;
+	size_t	src_len;
 	size_t	c;
 
 	c = 0;
-	i = ft_strlen(dest);
-	j = ft_strlen(src);
-	if (dstsize < i)
-		return (j + dstsize);
-	if (i < dstsize - 1 && dstsize > 0)
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (dstsize < dest_len)
+		return (src_len + dstsize);
+	if (dest_len < dstsize - 1 && dstsize > 0)
 	{
-		while (src[c] && i + c < dstsize - 1)
+		while (src[c] && dest_len + c < dstsize - 1)
 		{
-			dest[i + c] = src[c];
+			dest[dest_len + c] = src[c];
 			c++;
 		}
-		dest[i + c] = '\0';
+		dest[dest_len + c] = '\0';
 	}
-	return (j + i);
+	return (src_len + src_len);
 }
 /*
+-------
+return value
+dstsize > dest_length 
+return => src_length + dest_length(else: dstsize)
+---
+The ft_strlcat function concatenates strings safely, avoiding buffer overflows. It calculates the lengths of the destination and source strings and returns if the destination size is insufficient. In the concatenation loop, it copies characters from the source to the destination until the null terminator or the available space is exhausted. The function returns the total length of the concatenated string. 
+*/
+/*
+#include <stdio.h>
 int	main()
 {
 	size_t dstsize = 8;
